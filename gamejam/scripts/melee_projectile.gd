a95b93be9513d4b078a9662122cf3349be4310b7
Ide <Idehen.idahor@mni.thm.de>
2025-01-06 11:37:05 +0100

    Projectile Basis

---
+++ gamejam/scripts/melee_projectile.gd
@@ -0,0 +1,26 @@
+extends Area2D
+@export var damage: int
+@export var speed: float = 300.0
+var direction: Vector2 = Vector2.ZERO
+
+func _process(delta: float) -> void:
+	position += direction * speed * delta
+	#if not get_viewport_rect().has_point(global_position):
+		#queue_free()
+
+func set_direction(target_direction: Vector2) -> void:
+	direction = target_direction.normalized()
+
+
+func _on_body_exited(body: Node2D) -> void:
+	pass # Replace with function body.
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("players") or body.has_method("player"):  # Beispiel: prüfe, ob das Projektil einen Feind trifft.
+		body.take_damage(damage)  # Rufe eine Funktion im Feindobjekt auf.
+		queue_free()  # Entferne das Projektil.
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
