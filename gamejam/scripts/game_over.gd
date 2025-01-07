1c2124a6f7df8962db199eafada502f3a4436171
Kevin <kevin.klauser@mni.thm.de>
2025-01-07 11:31:41 +0100

    freeze fix

--- gamejam/scripts/game_over.gd
+++ gamejam/scripts/game_over.gd
@@ -29,15 +29,12 @@ func _process(delta: float) -> void:
 
 
 func _on_restart_pressed() -> void:
-	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/level.tscn")
 	
 
 func _on_main_menu_pressed() -> void:
-	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 
 
 func _on_quit_pressed() -> void:
-	get_tree().paused = false
 	get_tree().quit()
