3358041eb0b9ffab9381ca05b427e675ba2945eb
Julian <julian.dauenhauer@mni.thm.de>
2025-01-08 10:39:17 +0100

    successfully added music hehehe

--- gamejam/scripts/interface/battlemusic.gd
+++ gamejam/scripts/interface/battlemusic.gd
@@ -1,9 +1,26 @@
 extends AudioStreamPlayer
 
+const SONG = preload("res://assets/Sound/Kämpfende Räder sind krass (battle theme).mp3")
+const SONG2 = preload("res://assets/Sound/Kämpfende Räder sind krass (battel theme 2).mp3")
+const SONG3 = preload("res://assets/Sound/Kämpfende Räder sind krass (battle theme 3).mp3")
+
+var current_song = 0
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var nr = randi_range(0,2)
+	match nr:
+		0:
+			stream = SONG
+			current_song = 0
+		1:
+			stream = SONG2
+			current_song = 1
+		2:
+			stream = SONG3
+			current_song = 2
+	play()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,4 +29,21 @@ func _process(delta: float) -> void:
 	
 
 func _on_finished() -> void:
-	self.stream
+	match current_song:
+		0:
+			stream = SONG2
+			current_song = 1
+		1:
+			stream = SONG3
+			current_song = 2
+		2:
+			stream = SONG
+			current_song = 0
+	await get_tree().create_timer(1.0).timeout
+	play()
+
+func _muffle_music():
+	bus = &"Muffle"
+
+func _unmuffle_music():
+	bus = &"Master"
