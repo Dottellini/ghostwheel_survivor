f63ee08f7c510a9785cba8dd93aa1103ed6ee043
Ide <Idehen.idahor@mni.thm.de>
2025-01-09 14:50:50 +0100

    Font,Boss_spawn
    Font changed to scare em
    Boss spawn implemented

--- gamejam/scripts/globaly.gd
+++ gamejam/scripts/globaly.gd
@@ -1,7 +1,7 @@
 extends Node
 var scene_list: Array = []
 var array_size: int = scene_list.size()
-var buff: int
+var buff: int = 0
 var volume_in_db: float
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
