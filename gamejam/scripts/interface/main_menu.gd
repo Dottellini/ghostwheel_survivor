839384415697a8fbeb3f378f6d0eef58b7607fb3
BuildTools <unconfigured@null.spigotmc.org>
2025-01-10 15:14:24 +0100

    heilige scheiße es ist vollbracht

--- gamejam/scripts/interface/main_menu.gd
+++ gamejam/scripts/interface/main_menu.gd
@@ -1,5 +1,6 @@
 extends Node
 
+var credit_pos
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,6 +9,7 @@ func _ready() -> void:
 	Globaly.player = null
 	Globaly.got_player = false
 	Globaly.reset_global_state()
+	credit_pos = $credits/credts_text.global_position
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -29,3 +31,19 @@ func _on_controls_button_mouse_entered() -> void:
 
 func _on_controls_button_mouse_exited() -> void:
 	$Controls.hide()
+
+
+func _on_credits_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Interface/credits.tscn")
+
+
+func _on_credits_button_down() -> void:
+	$credits/credts_text.global_position.y += 5
+
+
+func _on_credits_button_up() -> void:
+	$credits/credts_text.global_position = credit_pos
+
+
+func _on_credits_mouse_exited() -> void:
+	$credits/credts_text.global_position = credit_pos
