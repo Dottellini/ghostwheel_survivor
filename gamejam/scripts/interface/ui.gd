be670471918ee107a6fe2c184d6dd40ccb370b6e
Kevin Klauser <kevin.klauser@mni.thm.de>
2025-01-10 14:49:25 +0100

    fix missing shop pause

--- gamejam/scripts/interface/ui.gd
+++ gamejam/scripts/interface/ui.gd
@@ -18,7 +18,8 @@ func _ready() -> void:
 		if node.name != "shop":
 			node.visible = false
 	await get_tree().create_timer(0.5).timeout
-
+	pause_manager.request_pause("shop") # initial pause for the first shop 
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	$ScoreText.text = "Score: %d" % player.SCORE
