28beda6162b51967c664810004ddbf78e1c91022
Kevin Klauser <kevin.klauser@mni.thm.de>
2025-01-10 09:45:21 +0100

    added mute button

--- gamejam/scripts/interface/main_menu.gd
+++ gamejam/scripts/interface/main_menu.gd
@@ -1,5 +1,7 @@
 extends Node
 
+var is_muted = false
+signal _mute_sound_toggle
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,6 +10,7 @@ func _ready() -> void:
 	Globaly.player = null
 	Globaly.got_player = false
 	Globaly.reset_global_state()
+	Globaly.is_sound_muted = is_muted
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -29,3 +32,11 @@ func _on_controls_button_mouse_entered() -> void:
 
 func _on_controls_button_mouse_exited() -> void:
 	$Controls.hide()
+
+func _on_mute_button_toggled(toggled_on: bool) -> void:
+	is_muted = !is_muted
+	Globaly.is_sound_muted = is_muted
+	if is_muted and $main_theme_player.playing:
+		$main_theme_player.stop()
+	elif !is_muted and !$main_theme_player.playing:
+		$main_theme_player.play()
